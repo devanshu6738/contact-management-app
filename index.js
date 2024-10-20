e8b459fe453ff3e76484c8755304a1df26a80170
devanshu6738 <devanshu9167@gmail.com>
2024-10-20 23:54:54 +0530

    #

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app=express();
 const PORT=3000;
 const db=require("./connect")
 const routes=require("./routes/contact")
+const path=require("path")
 
 // middleware
 app.use(express.json())
 app.set("view engine", "ejs"); 
-app.use(express.static("public"));
+app.set("views",path.resolve("./views"))
+
 app.use('/contact',routes)
+
 
 app.listen(PORT,(req,res)=>{
     console.log(`Server is running at port ${PORT}`)
--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const contactList = require("./../models/contact");
+const path=require("path");
 
 router.post('/', async (req, res) => {
     try {
         const data = req.body;
         const newContact = new contactList(data);
         const response = await newContact.save(); 
-        res.status(200).json(response);
+        res.status(200).json(response)
     } catch (error) {
         res.status(500).json({ message: "Error saving contact", error });
     }
@@ -17,11 +18,10 @@ router.get('/',async(req,res)=>{
     try {
         const data=await contactList.find();
         console.log("data fetched")
-        res.status(200).json(data);
-
+        res.status(200).render('index',{contacts:data})
     } catch (error) {
         console.log(error);
-        res.status(400).json({msg:"Internal server error"})
+        res.status(400).json({msg:"Intrenal server error"})
     }
 })
 router.put('/:id',async(req,res)=>{

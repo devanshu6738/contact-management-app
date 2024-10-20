90348c989fb9c9eb39c196766988d6fa13bdb311
devanshu6738 <devanshu9167@gmail.com>
2024-10-20 22:20:59 +0530

    #

--- routes/contact.js
+++ routes/contact.js
@@ -13,4 +13,43 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/',async(req,res)=>{
+    try {
+        const data=await contactList.find();
+        console.log("data fetched")
+        res.status(200).json(data);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:"Internal server error"})
+    }
+})
+router.put('/:id',async(req,res)=>{
+    try {
+        const data=req.params.id;
+        const updateData=req.body;
+        const response=await contactList.findByIdAndUpdate(data,updateData,{
+            new:true,
+            runValidators:true,
+    })
+    if(!updateData){
+        return res.status(404).json({error:"person not found"})
+    }
+    console.log("contact updated")
+    res.status(200).json(response);
+} catch (error) {
+        console.log(error)
+        res.status(500).json({error:"Interbal Server Error"})
+    }
+})
+router.delete('/:id', async (req, res) => {
+    try {
+        const data=req.params.id;
+        const deletedContact = await contactList.findByIdAndDelete(data);
+        if (!deletedContact) return res.status(404).json({ message: 'Contact not found' });
+        res.json({ message: 'Contact deleted' });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 module.exports = router;
